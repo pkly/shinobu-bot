987012c9e1575b88c84109172136ca84d540f76a
Ly <kakolp95@gmail.com>
2021-05-03 09:39:30 +0200

    Add model for deserialization of api commands

--- bot/src/Helper.cs
+++ bot/src/Helper.cs
@@ -1,12 +1,17 @@
 using System;
+using System.Collections.Generic;
+using System.IO;
 using System.Text.Json;
 using Disqord;
 using CommandLine;
+using Shinobu.Models.Assets;
 
 namespace Shinobu
 {
     class Helper
     {
+        private const string REACTION_COMMANDS_FILE = "reaction-commands-api.json";
+        
         public static long GetTimestamp()
         {
             return Convert.ToInt64((DateTime.Now.ToUniversalTime() - new DateTime(1970, 1, 1)).TotalMilliseconds);
@@ -19,12 +24,27 @@ namespace Shinobu
 
         public static void Init(string[] args)
         {
+            // this should prolly be moved
             Parser.Default.ParseArguments<Options>(args).WithParsed(o => {
-                var path = o.EnvPath == null ? null : o.EnvPath;
+                var path = o.EnvPath;
                 DotNetEnv.Env.LoadMulti(new[] {
                     path + "/.env",
                     path + "/.env.local"
                 });
+
+                var assetsPath = o.AssetsPath ?? "assets/";
+                if (!Directory.Exists(assetsPath))
+                {
+                    throw new Exception("Assets directory not found");
+                }
+
+                Console.WriteLine(File.ReadAllText(assetsPath + "/" + REACTION_COMMANDS_FILE));
+                var parsed = JsonSerializer.Deserialize<Dictionary<string, ApiCommand>>(File.ReadAllText(assetsPath + "/" + REACTION_COMMANDS_FILE));
+                foreach (var command in parsed)
+                {
+                    Console.WriteLine(command.Key);
+                    Console.WriteLine(command.Value.Group);
+                }
             });
         }
     }
---
+++ bot/src/Models/Assets/ApiCommand.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+
+namespace Shinobu.Models.Assets
+{
+    public class ApiCommand
+    {
+        // Api-specific
+        public string Url;
+        public string? ReplaceUrl = null;
+        public string Path = "url"; // default api path for the image
+
+        // Identifiers
+        public string Name;
+        public IList<string> Aliases;
+        
+        // Texts
+        public IList<string> Mention;
+        public IList<string> NoMention;
+
+        // Help parts
+        public string Group = "Unknown";
+        public int? SimpleGroup = null;
+    }
+}
